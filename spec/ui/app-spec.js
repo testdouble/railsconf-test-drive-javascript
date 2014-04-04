73afe74754b641b0f11fb47d83e4b13f14e647e8
Zach <briggszj@gmail.com>
2014-04-04 14:09:00 -0500

    Add high level checklist for what the UI needs.

---
+++ spec/ui/app-spec.js
@@ -0,0 +1,21 @@
+describe("What the UI needs to play battleship", function () {
+  it("can start a new game", function () {
+  });
+
+  it("can keep track of the phase.", function () {
+  });
+
+  context("during setup.", function () {
+    it("primary grid", function () {
+    });
+
+    it("ship list", function () {
+    });
+
+    it("selected ship", function () {
+    });
+  });
+
+  context("during play.", function () {
+  });
+});
