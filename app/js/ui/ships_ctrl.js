35cb9c5b720b7ff98caa688badf12a8c88907d87
Zach <briggszj@gmail.com>
2014-04-05 17:00:18 -0500

    Change status when ship is picked up.

--- app/js/ui/ships_ctrl.js
+++ app/js/ui/ships_ctrl.js
@@ -2,4 +2,16 @@ angular.module("app").controller("shipsCtrl", function ($scope) {
   $scope.ships = ships;
 
   $scope.pickUp = pickUp;
+
+  $scope.shipStatusClass = function(ship) {
+    switch(ship.location) {
+      case "docked":
+        return "bg-warning";
+      case "in hand":
+        return "bg-primary";
+      case "deployed":
+        return "bg-success";
+    }
+
+  };
 });
