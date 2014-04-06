2232c39813a847d964bfd47ba41b28d2558a75fd
Zach <briggszj@gmail.com>
2014-04-06 17:17:16 -0500

    Fix switch indentation

--- app/js/battleship/battleship.js
+++ app/js/battleship/battleship.js
@@ -15,17 +15,18 @@ function Game() {
     switch (coordinates[0]) {
       case (coordinates[0][0] + coordinates[1][1]):
         ys = yCoordinates.slice(yCoordinates.indexOf(coordinates[0][0]), yCoordinates.indexOf(coordinates[1][0])+1);
-      shipLocations[shipName] = ys.map(function(e) {
-        return e + coordinates[0].slice(1);
-      });
-      break;
+        shipLocations[shipName] = ys.map(function(e) {
+          return e + coordinates[0].slice(1);
+        });
+        break;
       case (coordinates[1][0] + coordinates[0][1]):
         xs = xCoordinates.slice(xCoordinates.indexOf(coordinates[0].slice(1)), xCoordinates.indexOf(coordinates[1].slice(1))+1);
-      shipLocations[shipName] = xs.map(function(e) {
-        return coordinates[0][0] + e;
-      });
-      break;
+        shipLocations[shipName] = xs.map(function(e) {
+          return coordinates[0][0] + e;
+        });
+        break;
     }
+
     return self;
   }
 
